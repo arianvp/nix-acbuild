c20fa93d4a00eb8a774974fd57da5f21c0db085b
Arian van Putten <aeroboy94@gmail.com>
2017-06-03 20:33:15 +0200

    Add example of nginx with mount point

---
+++ examples/nginx-mount.nix
@@ -0,0 +1,42 @@
+{ pkgs ? import <nixpkgs> {}, acbuildTool ? import ./../acbuild.nix {}}:
+let
+  nginxPort = 80;
+  nginxConf = pkgs.writeText "nginx.conf" ''
+  user root root;
+  daemon off;
+  events {}
+  http {
+    access_log /dev/stdout;
+    server {
+      listen ${toString nginxPort};
+      index index.html;
+      location / {
+        root /mnt/website;
+      }
+    }
+  }
+  '';
+in
+  acbuildTool.buildImage
+  {
+    name = "example.com/nginx";
+    app = {
+      user = "root";
+      group = "root";
+      exec = ["${pkgs.nginx}/bin/nginx" "-c" "${nginxConf}" ];
+      ports = [
+        {
+          name = "http";
+          protocol = "tcp";
+          port =  nginxPort;
+        }
+      ];
+      mountPoints = [
+        {
+          name = "website";
+          path = "/mnt/website";
+          readOnly = true;
+        }
+      ];
+    };
+  }
